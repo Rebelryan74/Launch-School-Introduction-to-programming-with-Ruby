68a67dd2183d5350050c94f218f891b0d38ea564
Ryan <ryantrentham74@yahoo.com>
2022-12-18 14:49:54 +0000

    exercises for the method chapter in Intro to ruby book

---
+++ methods/exercise_2.rb
@@ -0,0 +1,14 @@
+x = 2
+# this returns the value 2
+
+puts x = 2
+#this returns the value nil
+
+p name = "Joe"
+#this returns the value "Joe"
+
+four = "four"
+#this returns the value four
+
+print something = "nothing"
+#this returns the value nil
---
+++ methods/exercise_4.rb
@@ -0,0 +1,9 @@
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")
+
+#this will not print anything because once the code explicitly runs return it exits the method
